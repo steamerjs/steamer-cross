6fe7f7a57c9f9a08977f58fae80708472c18ef32
lcxfs1991@gmail.com <lcxfs1991@gmail.com>
2016-08-08 13:04:08 +0800

    add readme

--- README.md
+++ README.md
@@ -1,2 +1,82 @@
 ## steamer-cross
 cross domain communication for development
+
+
+## Features
+* Bi-drectional communication
+	- parent can send/broadcast message to children
+	- chidren can send/broadcast message to parent and other children
+* Chaining method
+	- ie, cross.register().send()
+
+
+## Usage
+### Parent Send Message to Children
+
+#### Send message to specific child
+```
+var cross = new Cross('project1', 'parent');
+cross.register('iframe1', document.getElementById('iframe1').contentWindow);
+
+cross.send('iframe1', "hello ifram1, from parent");
+```
+
+#### Broadcast message to all children
+```
+var cross = new Cross('project1', 'parent');
+// chaining methods
+
+var window1 = window.open('./window1.html');
+cross.register('iframe1', document.getElementById('iframe1').contentWindow)
+	 .register('iframe2', document.getElementById('iframe2').contentWindow)
+	 .register('window1', window1);
+
+cross.broadcast("broadcast message, from parent");
+```
+
+#### Listen to message
+```
+var cross = new Cross('project1', 'iframe1');
+cross.listen(function(msg, event) {
+	// do something here
+});
+```
+
+#### Send message to child B from child A
+```
+var cross = new Cross('project', 'iframe1');
+cross.send('iframe2', 'hello iframe2, from iframe1');
+```
+
+#### Clear registration
+```
+// unregister specific child
+var cross = new Cross('project', 'parent');
+cross.register('iframe1', document.getElementById('iframe1').contentWindow);
+cross.unregister('iframe1');
+
+// unregister all children
+cross.unregister();
+```
+
+### Child Send Message to Parent or Other Children
+#### Send message to parent
+```
+var cross = new Cross('project', 'iframe1');
+cross.send('parent', 'hello parent, from iframe1');
+```
+
+#### Send message to specific child
+```
+var cross = new Cross('project', 'iframe1');
+cross.send('iframe2', 'hello ifram2, from iframe1');
+```
+
+#### Broadcast message to parent and all children
+```
+var cross = new Cross('project', 'iframe1');
+cross.broadcast('broadcast from iframe1, project1');
+```
+
+## Demo
+Please checkout the repo, and take a look at test folder
---
+++ index.js
@@ -0,0 +1,217 @@
+"use strict";
+/**
+ * steamer-cross
+ * github: https://github.com/SteamerTeam/steamer-cross
+ * npm: https://www.npmjs.com/package/steamer-cross
+ * version: 0.1.0
+ * date: 2016.08.06
+ */
+
+/**
+ * [Cross]
+ * @param {String} projectName  [project name]
+ * @param {String} relationName [parent|window|iframe]
+ */
+
+function Cross(projectName, relationName) {
+	// encrypted key
+	this.className = "__SteamerCrossV0.1.0__";
+	// target iframes/windows storage
+	this.targets = {};
+	// project name
+	this.projectName = projectName || '';
+	// current iframe/window name
+	this.relationName = relationName || 'parent';
+
+	this.init();
+
+	return this;
+}
+
+/**
+ * [check postMessage]
+ */
+Cross.prototype.init = function () {
+	if (!window.postMessage) {
+		throw new Error('"steamer-cross: your browser does not support postMessage"');
+	}
+};
+
+/**
+ * [register target and window]
+ * @param  {String} targetName [target iframe/window name]
+ * @param  {Window} target     [window object]
+ * @return {Object}            [this]
+ */
+Cross.prototype.register = function (targetName, target) {
+	// projectName|targetName
+	var key = this.projectName + '|' + targetName;
+	if (!this.targets.hasOwnProperty(key)) {
+		var t = {
+			target: target
+		};
+		this.targets[key] = t;
+	}
+
+	return this;
+};
+
+/**
+ * [unreigster targets]
+ * @param  {String} targetName [target iframe/window name]
+ * @return {Object}            [this]
+ */
+Cross.prototype.unregister = function () {
+	var targetName = arguments.length <= 0 || arguments[0] === undefined ? null : arguments[0];
+
+	if (this.targets.hasOwnProperty(targetName)) {
+		delete this.targets[targetName];
+	}
+	// targetName = null, clear all targets
+	else if (!targetName) {
+			this.targets = {};
+		}
+
+	return this;
+};
+
+/**
+ * [encrypt message]
+ * @param  {String} projectTargetName [projectName + targetName]
+ * @param  {String} msg               [msg]
+ * @param  {String} direction         [ptc|ctp ptc => parent to children, ctp => children to parent]
+ * @param  {String} destinationName   [destination target name]
+ * @return {String}                   [encrypted message]
+ */
+Cross.prototype.encryptMsg = function (projectTargetName, msg) {
+	var direction = arguments.length <= 2 || arguments[2] === undefined ? 'ptc' : arguments[2];
+	var destinationName = arguments.length <= 3 || arguments[3] === undefined ? '' : arguments[3];
+
+	destinationName = destinationName ? '|' + destinationName : '';
+	// projectName|targetName|(ptc|ctp)(|destinationTargetName)|className|msg
+	return projectTargetName + '|' + direction + destinationName + this.className + msg;
+};
+
+/**
+ * [broadcast message all iframe/window]
+ * @param  {String} msg       [message]
+ * @param  {String} direction [ptc|ctp ptc => parent to children, ctp => children to parent]
+ * @return {Object}           [this]
+ */
+// ptc => parent to children ctp => children to parent
+Cross.prototype.broadcast = function (msg) {
+	var _this = this;
+
+	var direction = arguments.length <= 1 || arguments[1] === undefined ? 'ptc' : arguments[1];
+	var ignoreTargetName = arguments.length <= 2 || arguments[2] === undefined ? '' : arguments[2];
+
+	setTimeout(function () {
+		// current window is parent, start broadcasting
+		if (_this.relationName === 'parent') {
+			// prevent the broadcasting message sent by child sending back to this child again
+			var ignoreKey = _this.projectName + '|' + ignoreTargetName;
+			for (var key in _this.targets) {
+				if (key !== ignoreKey) {
+					var targetUrl = _this.targets[key].target.location.href;
+					_this.targets[key].target.postMessage(_this.encryptMsg(key, msg, direction), targetUrl);
+				}
+			}
+		}
+		// if not parent, send back to parent first, then parent will handle it
+		else {
+				_this.send('parent', msg, 'ctp', 'all|' + _this.relationName);
+			}
+	}, 0);
+
+	return this;
+};
+
+/**
+ * [send message to specific iframe/window]
+ * @param  {String} targetName      [target iframe/window name]
+ * @param  {String} msg             [message]
+ * @param  {String} direction [ptc|ctp ptc => parent to children, ctp => children to parent]
+ * @param  {String} destinationName   [destination target name]
+ * @return {Object}                 [this]
+ */
+Cross.prototype.send = function (targetName, msg) {
+	var _this2 = this;
+
+	var direction = arguments.length <= 2 || arguments[2] === undefined ? 'ptc' : arguments[2];
+	var destinationName = arguments.length <= 3 || arguments[3] === undefined ? '' : arguments[3];
+
+	setTimeout(function () {
+		var key = _this2.projectName + '|' + targetName;
+
+		if (_this2.targets.hasOwnProperty(key)) {
+			var targetUrl = _this2.targets[key].target.location.href;
+			_this2.targets[key].target.postMessage(_this2.encryptMsg(key, msg, direction, destinationName), targetUrl);
+		}
+		// if current window is not parent, send back to parent first to handle it
+		else if (_this2.relationName !== 'parent') {
+				_this2.send('parent', msg, 'ctp', targetName);
+			} else {
+				throw new Error("steamer-cross: targetName not exists");
+			}
+	}, 0);
+
+	return this;
+};
+
+/**
+ * [listen message]
+ * @param  {Function} callback [callback]
+ * @return {Object}            [this]
+ */
+Cross.prototype.listen = function (callback) {
+	var _this3 = this;
+
+	var listenFunc = function listenFunc(e) {
+
+		_this3.register('parent', e.source);
+
+		var msg = e.data;
+
+		// filter msg from other projects
+		if (!~msg.indexOf(_this3.projectName + '|' + _this3.relationName)) {
+			return;
+		}
+
+		msg = msg.split(_this3.className);
+
+		var info = msg[0] || '',
+		    data = msg[1] || '';
+		info = info.split('|');
+
+		// send from child to parent
+		// parent handle message sent from children here
+		if (_this3.relationName === 'parent' && info.length >= 4 && info[2] == 'ctp') {
+
+			if (info[3] !== 'all') {
+				// child A send message to child B
+				// info[3] => child B targetName
+				_this3.send(info[3], data);
+			}
+			// all => broadcast
+			else if (info[3] === 'all') {
+					// parent will also receive broadcasting message
+					callback(data, e);
+					// parent broadcast the message to other children
+					// info[4] => the child which broadcast message 
+					_this3.broadcast(data, 'ptc', info[4]);
+				}
+		} else {
+			callback(data, e);
+		}
+	};
+
+	if (window.addEventListener) {
+		window.addEventListener('message', listenFunc, false);
+	} else {
+		window.attachEvent('onmessage', listenFunc);
+	}
+
+	return this;
+};
+
+exports.default = Cross;
